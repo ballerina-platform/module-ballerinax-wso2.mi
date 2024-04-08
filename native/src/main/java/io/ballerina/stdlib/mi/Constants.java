1460f9858c1617f173838e354ce90a34ac1eac61
samithkavishke <samithkarunathilake@email.com>
2024-04-08 12:10:01 +0530

    removed unnecessary variables

--- native/src/main/java/io/ballerina/stdlib/mi/Constants.java
+++ native/src/main/java/io/ballerina/stdlib/mi/Constants.java
@@ -7,10 +7,6 @@ import java.util.ArrayList;
 public class Constants {
     public static final String RESULT = "result";
     public static final String CONTEXT = "context";
-    public static final String PAYLOAD = "payload";
-    public static final String FIRST_ARGUMENT = "firstArgument";
-    public static final String SECOND_ARGUMENT = "secondArgument";
-    public static final String FUNCTION_NAME = "functionName";
     public static final String ORG_NAME = "wso2";
     public static final String MODULE_NAME = "datamapper";
     public static final String VERSION = "0.1.0";
--- native/src/main/java/io/ballerina/stdlib/mi/Mediator.java
+++ native/src/main/java/io/ballerina/stdlib/mi/Mediator.java
@@ -11,26 +11,13 @@ import java.util.HashMap;
 
 public class Mediator extends SimpleMediator {
     static Scheduler scheduler = new Scheduler(false);
-    private String firstArgument = "";
-    private String secondArgument = "";
-    private String thirdArgument = "";
-    private String fourthArgument = "";
-    private String fifthArgument = "";
-    private String sixthArgument = "";
-    private String functionName = "transform_1";
 
     public void mediate(SimpleMessageContext context) {
         HashMap<String, Object> properties = new HashMap<String, Object>() {
             {
-                this.put("payload", Z.this.getPayload(context));
-                this.put("firstArgument", Z.this.getBXmlParameter(context, "param0"));
-                this.put("secondArgument", Z.this.getBXmlParameter(context, "param1"));
-                this.put("thirdArgument", Z.this.getThirdArgument());
-                this.put("fourthArgument", Z.this.getFourthArgument());
-                this.put("fifthArgument", Z.this.getFifthArgument());
-                this.put("sixthArgument", Z.this.getSixthArgument());
+                this.put("firstArgument", Mediator.this.getBXmlParameter(context, "param0"));
+                this.put("secondArgument", Mediator.this.getBXmlParameter(context, "param1"));
                 this.put("functionName", context.getProperty("paramFunctionName"));
-                this.put("xmlProperty", context.getProperty("xmlProperty"));
             }
         };
 
@@ -42,65 +29,4 @@ public class Mediator extends SimpleMediator {
     public BXml getBXmlParameter(SimpleMessageContext context, String parameterName) {
         return OMElementConverter.toBXml((OMElement) context.getProperty((String) context.getProperty(parameterName)));
     }
-
-    public void setFirstArgument(String value) {
-        this.firstArgument = value;
-    }
-
-    public String getFirstArgument() {
-        return this.firstArgument;
-    }
-
-    public void setSecondArgument(String value) {
-        this.secondArgument = value;
-    }
-
-    public String getSecondArgument() {
-        return this.secondArgument;
-    }
-
-    public void setThirdArgument(String value) {
-        this.thirdArgument = value;
-    }
-
-    public String getThirdArgument() {
-        return this.thirdArgument;
-    }
-
-    public String getFunctionName() {
-        return this.functionName;
-    }
-
-    public void setFunctionName(String functionName) {
-        this.functionName = functionName;
-    }
-
-    public BXml getPayload(SimpleMessageContext context) {
-        return OMElementConverter.toBXml(context.getRootXmlElement());
-    }
-
-    public String getFourthArgument() {
-        return this.fourthArgument;
-    }
-
-    public void setFourthArgument(String fourthArgument) {
-        this.fourthArgument = fourthArgument;
-    }
-
-    public String getFifthArgument() {
-        return this.fifthArgument;
-    }
-
-    public void setFifthArgument(String fifthArgument) {
-        this.fifthArgument = fifthArgument;
-    }
-
-    public String getSixthArgument() {
-        return this.sixthArgument;
-    }
-
-    public void setSixthArgument(String sixthArgument) {
-        this.sixthArgument = sixthArgument;
-    }
-
 }
